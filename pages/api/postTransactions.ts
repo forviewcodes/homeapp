15ebae6f98e1806c0838e9d619ba034a8ed70956
harrisazmi <azmi.harris@gmail.com>
2024-11-28 14:34:29 +0800

    chore : initial stable backend

---
+++ pages/api/postTransactions.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URL;
+if (!uri) {
+  throw new Error("MONGODB_URL environment variable is not set");
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await client.connect();
+    const db = client.db("budgetDatabase");
+    const result = await db.collection("DailyTransaction").insertOne(req.body);
+    return res
+      .status(201)
+      .json({ message: "Transaction added", id: result.insertedId });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to add transaction" });
+  } finally {
+    await client.close();
+  }
+}
---
+++ pages/api/test.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URL;
+if (!uri) {
+  throw new Error("MONGODB_URL environment variable is not set");
+}
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    const dbInfo = await client.db().admin().listDatabases();
+
+    res.status(200).json({
+      message: "Successfully connected to MongoDB!",
+      databases: dbInfo.databases,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to connect to MongoDB" });
+  } finally {
+    await client.close();
+  }
+}
